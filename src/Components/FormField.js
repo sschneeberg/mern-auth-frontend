824f5f6e25db934ed14ed9725488d977aaa17a76
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-18 16:26:51 -0500

    Build out SignUp

---
+++ src/Components/FormField.js
@@ -0,0 +1,10 @@
+function FormField(props) {
+    return (
+        <div className="form-field">
+            <label htmlFor={props.label}>{props.display}</label>
+            <input type={props.type} value={props.value} name={props.label} onChange={props.onChange} className="form-control" />
+        </div>
+    );
+}
+
+export default FormField;
