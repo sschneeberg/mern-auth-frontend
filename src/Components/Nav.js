1e230d46dc1f133fe27ebca60ca48f4d36a5201a
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-18 12:39:32 -0500

    Build out Nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
-function Nav() {
+function Nav(props) {
     return (
-        <nav>
-            <p>Navbar</p>
-            <ul></ul>
-        </nav>
+        <div className="container">
+            <Link className="navbar-brand" to="/">
+                MERN Auth
+            </Link>
+            <button
+                className="navbar-toggle"
+                type="button"
+                data-toggle="#navbarsExample07"
+                data-target="#navbarsExample07"
+                aria-controls="#navbarExample07"
+                aria-label="Toggle Navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarsExample07">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" exact to="/">
+                            Home
+                        </NavLink>
+                        <NavLink className="nav-link" exact to="/about">
+                            About
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 }
+
+export default Nav;
