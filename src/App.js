6a4d2f28cca32a8422017a155c7fca45e09bf577
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-18 12:07:54 -0500

    Add and Test Welcome component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
+import Welcome from './Components/Welcome';
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Welcome />
+        </div>
+    );
 }
 
 export default App;
